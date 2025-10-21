4d72d9e32ecb76e5269da8fd52e33c17a4894848
Lozano Mora Diego <lozano.mora.diego@gmail.com>
2025-10-21 17:48:20 -0500

    refactor: refactorizacion de el css para añadirle un :root

--- src/FRONTEND/pages/Home.jsx
+++ src/FRONTEND/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Headers from '../componentes/componente_General/header'
+import Footer from '../componentes/componente_General/footer'
 
 function Home() {
   return (
     <div>
       <Headers />
+      <Footer />
     </div>
   )
 }
--- src/FRONTEND/pages/MisJuegos.jsx
+++ src/FRONTEND/pages/MisJuegos.jsx
@@ -1,10 +1,12 @@
 import Headers from '../componentes/componente_General/header'
+import Footer from '../componentes/componente_General/footer'
 
 function MisJuegos() {
   return (
     <div>
       <Headers />
       <h1>Juegos</h1>
+      <Footer />
     </div>
   )
 }
