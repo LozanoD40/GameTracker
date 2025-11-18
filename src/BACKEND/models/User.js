fe0dcbb301d543ecd5b01526ede0c5f4edacff7c
Lozano Mora Diego <lozano.mora.diego@gmail.com>
2025-11-18 15:30:44 -0500

    Fix: arreglo de publicacion

--- src/BACKEND/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const userSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  contrasenia: { type: String, required: true },
-})
-
-export default mongoose.model('User', userSchema)
